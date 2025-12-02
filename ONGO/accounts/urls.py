930cd9e6f183e2baba4e249585480a8dc605ab40
Don Soby <donkiran2255@gmail.com>
2025-12-02 21:54:37 +0530

    completed : admin-login redirectd to dashboard

--- ONGO/accounts/urls.py
+++ ONGO/accounts/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/login', views.admin_login_view, name='admin_login')
+    path('admin/login/', views.admin_login_view, name='admin_login'),
+    path('admin/dashboard/', views.admin_dashboard_view, name='admin_dashboard'),
+
+    path('', views.user_login_view, name='user_login'),
+    path('signup/', views.user_signup_view, name='user_signup'),
 ]
--- ONGO/accounts/views.py
+++ ONGO/accounts/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-
 # Create your views here.
 
 
 def admin_login_view(request):
     return render(request, 'accounts/admin-login.html')
+
+
+def admin_dashboard_view(request):
+    return render(request, 'accounts/admin-dashboard.html')
+
+
+def user_login_view(request):
+    return render(request, 'accounts/user-login.html')
+
+
+def user_signup_view(request):
+    return render(request, 'accounts/user-sigup.html')
