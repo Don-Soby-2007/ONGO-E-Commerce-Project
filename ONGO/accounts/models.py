67a6e7f0fe088c8f89ace6c958697d698b3c5b7b
Don Soby <donkiran2255@gmail.com>
2025-12-02 13:18:44 +0530

    user model created

--- ONGO/accounts/models.py
+++ ONGO/accounts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+def user_profile_picture_path(instance, filename):
+    return f'profile_pictures/user_{instance.id}/{filename}'
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=10, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to=user_profile_picture_path, blank=True, null=True)
+
+    is_blocked = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email or self.username
