de2a606e4c42bd80c511ebe325b5056f3cafa3ac
Don Soby <donkiran2255@gmail.com>
2025-12-06 18:59:48 +0530

    pages are loaded

--- ONGO/ONGO/urls.py
+++ ONGO/ONGO/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('jsaldfhiuasdhf-admin/', admin.site.urls),
     path('', include('accounts.urls')),
+    path('admin/', include('adminpanel.urls')),
 ]
 
 if settings.DEBUG:
--- ONGO/adminpanel/urls.py
+++ ONGO/adminpanel/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/login/', views.admin_login_view, name='admin_login'),
-    path('/dashboard/', views.admin_customer_view, name='dashboard')
+    path('login/', views.admin_login_view, name='admin_login'),
+    path('customers/', views.admin_customer_view, name='customers')
 ]
