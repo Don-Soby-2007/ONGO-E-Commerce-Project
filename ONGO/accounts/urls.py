9060c6c1811bc28a9c2e99e581fd2af2b698185b
Don Soby <donkiran2255@gmail.com>
2025-12-14 05:07:33 +0530

    created: password_reset odne and completed

--- ONGO/accounts/urls.py
+++ ONGO/accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
@@ -10,6 +11,46 @@ urlpatterns = [
     path('user-confirmed/', views.userConfirmedView, name='user_confirmed'),
 
     path('', views.homeView, name='home'),
-    path('logout/', views.userLogoutView, name='logout')
+    path('logout/', views.userLogoutView, name='logout'),
+
+    path(
+        'password-reset/',
+        auth_views.PasswordResetView.as_view(
+            template_name='accounts/password_reset.html'
+        ),
+        name='password_reset'
+    ),
+
+    path(
+        'password-reset/done/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='accounts/password_reset_done.html'
+        ),
+        name='password_reset_done'
+    ),
+
+    path(
+        'password-reset-confirm/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='accounts/password_reset_confirm.html'
+        ),
+        name='password_reset_confirm'
+    ),
+
+    path(
+        'password-reset-complete/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='accounts/password_reset_complete.html'
+        ),
+        name='password_reset_complete'
+    ),
 
 ]
+
+
+urlpatterns = [
+    
+]
+
+
+if i use toast then what is the use of  done and complete view                                                                                                                                                                                                                                                                                                                                                                            
