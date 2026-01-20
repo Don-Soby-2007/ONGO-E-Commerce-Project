86ab5b17fc090604bb45f4c3b1c50ef28a06adfa
Don Soby <donkiran2255@gmail.com>
2026-01-20 21:24:32 +0530

    Fix: routes in order information address adding

--- ONGO/order/urls.py
+++ ONGO/order/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('information/', views.orderInformation, name='checkout_information')
+    path('information/', views.orderInformation, name='checkout_information'),
+    path('payment-methode/', views.paymentMethode, name='payment_methode')
 ]
--- ONGO/order/views.py
+++ ONGO/order/views.py
@@ -1,5 +1,5 @@
-# from django.shortcuts import redirect
 from django.shortcuts import render, redirect
+from django.contrib import messages
 # from django.views.generic import ListView
 # from .models import Cart
 # from django.contrib.auth.mixins import LoginRequiredMixin
@@ -14,6 +14,8 @@ from django.shortcuts import render, redirect
 # from django.views.decorators.csrf import csrf_exempt
 
 from django.contrib.auth.decorators import login_required
+
+from accounts.utils import create_address_from_request
 
 # import json
 
@@ -32,3 +34,24 @@ def orderInformation(request):
         return redirect('login')
 
     return render(request, 'checkout/information.html')
+
+
+@login_required
+def paymentMethode(request):
+
+    if not request.user.is_authenticated:
+        return redirect('login')
+
+    return render(request, 'checkout/payment.html')
+
+
+@login_required
+def add_address_in_checkout(request):
+    if request.method == 'POST':
+        success, message, _ = create_address_from_request(request)
+        if success:
+            messages.success(request, message)
+            return redirect('checkout_information')
+        else:
+            messages.error(request, message)
+    return render(request, 'accounts/partials/add_address_form.html')
