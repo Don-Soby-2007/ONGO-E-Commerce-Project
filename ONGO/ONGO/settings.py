3ba785d7ca080cefd8d7ad5c59404c564a58a0fc
Don Soby <donkiran2255@gmail.com>
2026-01-20 20:13:17 +0530

    Created: Model order & oderitem

--- ONGO/ONGO/settings.py
+++ ONGO/ONGO/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'adminpanel',
     'products',
     'cart',
+    'order',
 ]
 
 MIDDLEWARE = [
--- ONGO/order/models.py
+++ ONGO/order/models.py
@@ -1,3 +1,57 @@
+import uuid
 from django.db import models
+from accounts.models import User, Address
+from django.core.validators import MinValueValidator
+from products.models import ProductVariant
 
-# Create your models here.
+
+class Order(models.Model):
+
+    order_id = models.UUIDField(
+        default=uuid.uuid4,
+        editable=False,
+        unique=True,
+        help_text="Public unique identifier for the order"
+    )
+
+    ORDER_STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('confirmed', 'Confirmed'),
+        ('shipped', 'Shipped'),
+        ('delivered', 'Delivered'),
+        ('cancelled', 'Cancelled'),
+        ('failed', 'Failed'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    address = models.ForeignKey(Address, on_delete=models.PROTECT)
+    sub_total = models.DecimalField(max_digits=12, decimal_places=2)
+    discount_amount = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    total_amount = models.DecimalField(max_digits=12, decimal_places=2)
+    status = models.CharField(max_length=20, choices=ORDER_STATUS_CHOICES, default='pending')
+    coupon_code = models.CharField(max_length=50, blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Order {self.order_id} - {self.user.email}"
+
+    class Meta:
+        ordering = ['-created_at']
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product_variant = models.ForeignKey(ProductVariant, on_delete=models.PROTECT)
+
+    product_name = models.CharField(max_length=255)
+    variant_options = models.JSONField()
+    image_url = models.URLField()
+    price_at_time_of_order = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    total_price = models.DecimalField(max_digits=12, decimal_places=2)
+
+    def save(self, *args, **kwargs):
+        self.total_price = self.quantity * self.price_at_time_of_order
+        super().save(*args, **kwargs)
