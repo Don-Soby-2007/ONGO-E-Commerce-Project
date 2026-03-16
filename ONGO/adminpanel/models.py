d9850bd18357ecf94766e374a06f4aae655923ab
Don Soby <donkiran2255@gmail.com>
2026-03-16 21:40:09 +0530

    Update: Banner model created

--- ONGO/adminpanel/models.py
+++ ONGO/adminpanel/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Banner(models.Model):
+
+    LOCATION_CHOICES = [
+        ("home_hero", "Home Hero"),
+        ("home_secondary", "Home Secondary"),
+        ("category_top", "Category Top"),
+    ]
+
+    title = models.CharField(max_length=200)
+
+    desktop_image = models.URLField()
+    mobile_image = models.URLField(blank=True, null=True)
+
+    redirect_link = models.URLField(blank=True)
+
+    location = models.CharField(max_length=50, choices=LOCATION_CHOICES)
+
+    priority = models.IntegerField(default=1)
+
+    start_date = models.DateTimeField(blank=True, null=True)
+    end_date = models.DateTimeField(blank=True, null=True)
+
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'banners'
+
+    def __str__(self):
+        return f"banner for {self.title} in {self.location}"
