8bd5320510bb9a74f2e1b6740665717bc4b87816
Don Soby <donkiran2255@gmail.com>
2026-03-23 15:36:54 +0530

    Fix: Allowed host

--- ONGO/ONGO/settings.py
+++ ONGO/ONGO/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['43.205.177.149', 'localhost', '127.0.0.1']
 
 SECURE_CROSS_ORIGIN_OPENER_POLICY = "same-origin-allow-popups"
 
